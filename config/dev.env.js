bbbcb668302baee797e9b7eb8fb001c7fb47623a
张京京 <1765559690@qq.com>
2017-12-13 11:42:42 +0800

    更新接口（线上、测试、本地开发）

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: JSON.stringify('development')
+  NODE_ENV: JSON.stringify('development'),
+  STORE_SERVER: JSON.stringify('http://192.168.31.192:2080'),
+  SAAS_SERVER: JSON.stringify('http://0.0.0.0:40080')
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: JSON.stringify('production')
+  NODE_ENV: JSON.stringify('production'),
+  STORE_SERVER: JSON.stringify('http://m.jingfree.com/api'),
+  SAAS_SERVER: JSON.stringify('http://sapi.jingfree.com')
 }
