6fc7999802f2c37d815c6188380bc2b3d0c86b16
张京京 <1765559690@qq.com>
2017-12-13 11:50:48 +0800

    新增接口

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: JSON.stringify('production'),
+  DOMAIN: JSON.stringify('.jingfree.com'),
   STORE_API: JSON.stringify('http://m.jingfree.com/api'),
   SAAS_API: JSON.stringify('http://sapi.jingfree.com')
 }
--- config/test.env.js
+++ config/test.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: JSON.stringify('test'),
+  DOMAIN: JSON.stringify('.loveqiche.com'),
   STORE_API: JSON.stringify('http://m.loveqiche.com/api'),
   SAAS_API: JSON.stringify('http://120.27.144.62:40080')
 }
