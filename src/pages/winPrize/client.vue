6249cad216875bd351a2f4008c530f9ba00a7dab
张京京 <1765559690@qq.com>
2018-01-19 16:02:31 +0800

    更新金币和头像轮播，完成倒计时效果

--- src/pages/winPrize/client.vue
+++ src/pages/winPrize/client.vue
@@ -3,7 +3,7 @@
     <img class="abs slogan" :src="imgServerUrl + '/pages/win_prize/client_slogan.png'" alt="">
     <div class="abs info">
       <div class="people-info">
-        已有<span class="num">8,089,101</span>人闯关成功
+        已有<span class="num"> 8,089,101 </span>人闯关成功
       </div>
       <div class="user-swiper-wrap clearfix">
         <div class="user-content first clearfix">
@@ -16,6 +16,15 @@
             {{index}}
           </div>
         </div>
+      </div>
+    </div>
+    <div class="abs question">
+      <div class="abs clock">
+        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+          <circle id="progress1" r="40%" cy="50%" cx="50%" stroke-width="15" stroke="#ffd941" fill="none"/>
+          <circle id="progress2" r="40%" cy="50%" cx="50%" stroke-width="15" stroke="#d2d2d2" fill="none"/>
+        </svg>
+        <div class="clock-text">{{clockOpts.text}}</div>
       </div>
     </div>
     <div v-for="i in 9" v-bind:key="i" v-bind:class="'coin-'+i" class="coin"></div>
@@ -38,6 +47,11 @@ export default {
       headImgAnimate: {
         hideIndex1: 9,
         hideIndex2: -1
+      },
+      clockOpts: {
+        text: "05:00",
+        sumSecs: 300,
+        n: -360 / 300  //每秒转的圆心角度
       }
     }
   },
@@ -64,6 +78,7 @@ export default {
   mounted(){
     document.querySelector('.client-wrap').style.height = window.innerHeight + 'px';
     this.loopHeader(1, 'first');
+    this.initClock();
   },
   methods: {
     getWxUserInfo(){
@@ -74,16 +89,48 @@ export default {
         console.log('err')
       })
     },
+    initClock(){
+      // 设置半径
+      let c = $("svg").width() * 0.4 * Math.PI *2;
+      $("#progress1").css('stroke-dasharray',c);
+      $("#progress2").css('stroke-dasharray',c);
+      this.clock(1);
+    },
+    clock(sec){
+      let that = this;
+      let c = $("svg").width() * 0.4 * Math.PI *2;
+      if(sec > this.sumSecs){
+        return ;
+      }
+
+      // 设置时间
+      let nowTime = 300 -sec;
+      let nowTimeMin = Math.floor(nowTime / 60);
+      let nowSec = nowTime % 60;
+      if(nowSec < 10){
+        nowSec = "0" + nowSec;
+      }
+      this.clockOpts.text = "0" + nowTimeMin + ":" + nowSec;
+
+      // 设置时间进度条
+      this.clockOpts.n = this.clockOpts.n - 1.2;
+      let offset = (this.clockOpts.n * Math.PI * $("svg").width() * 0.4) / 180
+      $("#progress2").css('stroke-dashoffset', offset);
+
+      sec++;
+      setTimeout(function(){
+        that.clock(sec);
+      }, 1000)
+    },
     loopHeader(loop, type){
       let that = this;
       if(type == 'first'){
         if(loop > 20){
           that.headImgAnimate.hideIndex1 = -1;
-
-          $(".user-content.first").css("left", "250px");
+          $(".user-content.first").css("left", "625px");
           return;
         }
-        // if(loop == 12){
+
         if(loop == 12){
           that.loopHeader2(1, 'second');
         }
@@ -94,13 +141,12 @@ export default {
 
         if(loop > 29){
           that.headImgAnimate.hideIndex1 = -1;
-
-          $(".user-content.first").css("left", "250px");
+          $(".user-content.first").css("left", "625px");
           return;
         }
       }
 
-      $(".user-content.first").animate({'left': '-=25px'}, 0, 'linear')
+      $(".user-content.first").animate({'left': '-=57.5px'}, 0, 'linear')
 
       if(type == 'first'){
         $(".user-content.first .user").eq(loop + 8).removeClass('hide');
@@ -131,12 +177,12 @@ export default {
         if(loop > 29){
           that.headImgAnimate.hideIndex2 = -1;
 
-          $(".user-content.second").css("left", "250px");
+          $(".user-content.second").css("left", "625px");
           return;
         }
       }
 
-      $(".user-content.second").animate({'left': '-=25px'}, 0, 'linear')
+      $(".user-content.second").animate({'left': '-=57.5px'}, 0, 'linear')
 
       if(type == 'first'){
         $(".user-content.second .user").eq(loop + 8).removeClass('hide');
@@ -174,14 +220,14 @@ export default {
       margin: 0 auto;
     }
     .slogan{
-      top: 13%;
+      top: 14%;
       width: 84%;
     }
     .info{
-      top: 24%;
+      top: 25%;
       .people-info{
         color: #fff;
-        font-size: 14px;
+        font-size: 42px;
         letter-spacing: 1px;
         .num{
           color: #ffe221
@@ -189,30 +235,37 @@ export default {
       }
       .user-swiper-wrap{
         position: relative;
-        width: 280px;
-        height: 30px;
+        // width: 280px;
+        // height: 30px;
+        width: 700px;
+        height: 75px;
         margin: 0 auto;
-        margin-top: 5px;
+        margin-top: 10px;
         overflow: hidden;
         .user-content{
           position: absolute;
-          // width: 505px;
-          width: 530px;
+          // width: 530px;
+          width: 1330px;
           transition: left 1s;
           &.first{
-            left: 25px;
+            // left: 25px;
+            left: 100px;
           }
           &.second{
-            left: 250px;
+            // left: 250px;
+            left: 615px;
           }
           .user{
-            width: 30px;
-            height: 30px;
+            // width: 30px;
+            // height: 30px;
+            width: 70px;
+            height: 70px;
             float: left;
             opacity: 1;
             transform: scale(1);
             border-radius: 50%;
-            margin-left: -5px;
+            // margin-left: -5px
+            margin-left: -12.5px;
             transition: all .3s .1s;
            background-color: red;
             &.hide{
@@ -223,8 +276,47 @@ export default {
         }
       }
     }
+    .question{
+      top: 36%;
+      width: 84%;
+      z-index: 2;
+      background: #fff;
+      border-radius: 5%;
+      padding-bottom: 88%;
+      .clock{
+        top: -5%;
+        width: 24%;
+        z-index: 2;
+        padding-bottom: 24%;
+        border-radius: 50%;
+        background-color: #fff;
+        svg{
+          position: absolute;
+          margin: 0 auto;
+          left: 0;
+          right: 0;
+          z-index: 1;
+        }
+        .clock-text{
+          position: absolute;
+          height: 57px;
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          margin: auto;
+          font-size: 50px;
+          z-index: 2;
+        }
+        #progress2 {
+          transform: rotate(-90deg);
+          transform-origin: center;
+        }
+      }
+    }
     .coin{
       position: fixed;
+      z-index: 3;
       transition: all 2s;
       animation-fill-mode: forwards;
       background-image: url("@{IMAGE_SERVER}/pages/win_prize/coin.png");
@@ -233,59 +325,59 @@ export default {
         left: 0;
         right: 0;
         margin: 0 auto;
-        width: 24.9px;
-        height: 22.4px;
-        background-size: 124.5px 22.4px;
+        width: 74.7px;
+        height: 67.2px;
+        background-size: 373.5px 67.2px;
         animation: playgif .5s infinite steps(4), down 5s infinite linear;
         transform: rotate3d(1, 1, 0, 40deg)
       }
       &.coin-2{
         top: 10%;
         right: 30%;
-        width: 35.5px;
-        height: 32px;
+        width: 88.75px;
+        height: 80px;
         opacity: 0;
-        background-size: 177.5px 32px;
+        background-size: 443.75px 80px;
         animation: playgif .5s infinite steps(4), down 3s 3s infinite linear;
         transform: rotateX(-50deg) rotateY(50deg)
       }
       &.coin-3{
         top: 10%;
         left: 30%;
-        width: 35.5px;
         opacity: 0;
-        height: 32px;
-        background-size: 177.5px 32px;
+        width: 124.25px;
+        height: 112px;
+        background-size: 621.25px 112px;
         animation: playgif .5s infinite steps(4), down 4s 2s infinite linear;
         transform: rotateX(-10deg) rotateY(10deg)
       }
       &.coin-4{
         top: 10%;
         left: 10%;
-        width: 35.5px;
         opacity: 0;
-        height: 32px;
-        background-size: 177.5px 32px;
+        width: 113.6px;
+        height: 102.4px;
+        background-size: 568px 102.4px;
         animation: playgif .5s infinite steps(4), down 3s 4s infinite linear;
         transform: rotateX(-20deg) rotateY(20deg)
       }
       &.coin-5{
         top: 10%;
         left: 20%;
-        width: 35.5px;
         opacity: 0;
-        height: 32px;
-        background-size: 177.5px 32px;
+        width: 124.25px;
+        height: 112px;
+        background-size: 621.25px 112px;
         animation: playgif .5s infinite steps(4), down 3.5s 1.5s infinite linear;
         transform: rotateX(-10deg) rotateY(50deg)
       }
       &.coin-6{
         top: 10%;
         right: 5%;
-        width: 35.5px;
         opacity: 0;
-        height: 32px;
-        background-size: 177.5px 32px;
+        width: 74.7px;
+        height: 67.2px;
+        background-size: 373.5px 67.2px;
         animation: playgif .5s infinite steps(4), down 5s .5s infinite linear;
         transform: rotateX(0deg) rotateY(50deg)
       }
