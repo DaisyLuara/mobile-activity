f0403fc241009472f646c8254208bbdbdc0effba
张杨 <zhangyang1009@gmail.com>
2018-01-30 19:09:51 +0800

    add with cookie

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import axios from 'axios';
-
+axios.defaults.withCredentials = true ;
 Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
