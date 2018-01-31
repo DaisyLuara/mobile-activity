97a2abd2e6e34a1d900b25c97f767179db554f2a
Joy <joywangx@sina.com>
2018-01-31 10:19:05 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import axios from 'axios';
-axios.defaults.withCredentials = true ;
+// axios.defaults.withCredentials = true ;
 Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
-
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV == 'production') {
+  axios('http://sapi.xingstation.com/', {
+    withCredentials: true
+  });
+}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
