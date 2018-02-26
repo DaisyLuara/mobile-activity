655cc42a7fb82f023bb3e165bc51b31f7b04b5f1
张京京 <1765559690@qq.com>
2018-02-26 16:00:25 +0800

    update api config

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,9 +4,9 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: JSON.stringify('development'),
-  M_URL: JSON.stringify('http://127.0.0.1:8084'),
-  STORE_API: JSON.stringify('http://192.168.31.192:2080'),
-  SAAS_API: JSON.stringify('http://0.0.0.0:40080/api'),
-  WX_API: JSON.stringify('http://0.0.0.0:40080/api'),
+  M_URL: JSON.stringify('http://127.0.0.1:8087'),
+  STORE_API: JSON.stringify('http://store.jingfree.top/admin'),
+  SAAS_API: JSON.stringify('http://sapi.jingfree.top'),
+  WX_API: JSON.stringify('http://sapi.jingfree.top'),
   IMAGE_SERVER: JSON.stringify('http://h5-images.oss-cn-shanghai.aliyuncs.com')
 })
