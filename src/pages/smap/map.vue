325b44cd7de31033fd7b6a86e9620ba4b5bf1dbc
Joy <joywangx@sina.com>
2018-02-27 10:09:45 +0800

    Update

--- src/pages/smap/map.vue
+++ src/pages/smap/map.vue
@@ -2,13 +2,6 @@
   <div class="map-wrap"></div>
 </template>
 <script>
-// import THREE from './three.min.js';
-// import Detector from './Detector.js';
-// import OrbitControls from './OrbitControls.js';
-// import IndoorMap from './IndoorMap.js';
-// import Projector from './Projector.js';
-// import IndoorMap3d from './IndoorMap3d.js';
-// import Theme from './Theme.js';
 export default {
   components: {
   },
@@ -27,7 +20,7 @@ export default {
     dim: "3d"
   }
   var map = IndoorMap(params);
-  map.load('static/data/mapData.json', function(){
+  map.load('static/smap/data/mapData.json', function(){
     map.showFloor(1);
     map.setSelectable(false);
     map.showPubPoints(true);
