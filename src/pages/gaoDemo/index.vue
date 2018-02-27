57bef5a121c07bbf9a5331a56d385b0ea213c402
张京京 <1765559690@qq.com>
2018-02-27 11:39:27 +0800

    点击获取标记点pois信息

--- src/pages/gaoDemo/index.vue
+++ src/pages/gaoDemo/index.vue
@@ -32,37 +32,45 @@ export default {
       //     that.gaodeMap.indoorMap.showIndoorMap('B000A856LJ',5);
       // })
       // 方式二创建室内地图，控件形式
-      AMap.plugin(['AMap.IndoorMap', 'AMap.Geocoder'], function() {
+      AMap.plugin(['AMap.IndoorMap', 'AMap.Geocoder', 'AMap.PlaceSearch'], function() {
         var indoorMap = new AMap.IndoorMap({alwaysShow:true});
         //设定在没有矢量底图的时候也显示，默认情况下室内图仅在有矢量底图的时候显示
         that.gaodeMap = new AMap.Map('mapContainer', {
             zoom: 18,
             resizeEnable: false,
             showIndoorMap:false,//隐藏地图自带的室内地图图层
-            layers:[indoorMap,new AMap.TileLayer()]
+            layers:[indoorMap]
         });
 
         indoorMap.on('click', function(result){
-          // console.log(result)
+          console.log(result)
           // 新建地址查询
           let geoCoder = new AMap.Geocoder();
+
+          // 新建店铺查询
+          let shopSearch = new AMap.PlaceSearch();
 
           // 新建信息窗体
           let infoWindow = new AMap.InfoWindow();
 
           let address = '';
-          let lnglat = [result.lnglat.lng, result.lnglat.lat]
-          geoCoder.getAddress(lnglat,function(status,result){
-            if (status === 'complete' && result.info === 'OK') {
-              console.log(result.regeocode)
-              address = result.regeocode.formattedAddress; //返回地址描述
-              infoWindow.setContent(address);
-              infoWindow.open(that.gaodeMap, lnglat);
+          let shopPoiId = result.shop.poiId;
+          let lnglat = [result.lnglat.lng, result.lnglat.lat];
+          geoCoder.getAddress(lnglat,function(status,geoResult){
+            if (status === 'complete' && geoResult.info === 'OK') {
+              address = geoResult.regeocode.formattedAddress; //返回地址描述
+              shopSearch.getDetails(shopPoiId, function(status, shopResult){
+                if (status === 'complete' && shopResult.info === 'OK') {
+                  let poiArr = shopResult.poiList.pois;
+                  infoWindow.setContent(that.createContent(poiArr[0]));
+                  infoWindow.open(that.gaodeMap, lnglat);
+                }
+              })
             }
           })
         })
         indoorMap.showLabels();
-        indoorMap.showIndoorMap('B000A856LJ',2,"GD0000080210100027"); //building_id, 楼层，shop_id
+        indoorMap.showIndoorMap('B0FFF3ATUI',1); //building_id, 楼层，shop_id
       });
     },
     initIndoor(){
@@ -74,6 +82,14 @@ export default {
       map.once('mapready',function(){
         console.log(map.getFloor());
       })
+    },
+    createContent(poi) {
+        let s = [];
+        s.push("<b>名称：" + poi.name+"</b>");
+        s.push("地址：" + poi.address);
+        s.push("电话：" + poi.tel);
+        s.push("类型：" + poi.type);
+        return s.join("<br>");
     }
   },
 }
