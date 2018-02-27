d691945ed8a0fa227431a8974d6b322c17f35cb1
张京京 <1765559690@qq.com>
2018-02-27 10:02:42 +0800

    更新高德demo

--- src/pages/gaoDemo/index.vue
+++ src/pages/gaoDemo/index.vue
@@ -16,34 +16,64 @@ export default {
   mounted() {
     $('.gaode-wrap').css('height', $(window).height());
     this.initGaode();
+    // this.initIndoor();
   },
   created() {
   },
   methods: {
     initGaode(){
       let that = this;
-      this.gaodeMap = new AMap.Map('mapContainer', {
-        // resizeEnable: true,
-        // floorControl: true,
-        // floor: 1,
-        showIndoorMap: true,
-        center:[116.518542, 39.924677],
-        // zoom:20
-      });
+      // 方式一创建室内地图
+      // this.gaodeMap = new AMap.Map('mapContainer', {
+      //   showIndoorMap: true,
+      //   center:[116.518542, 39.924677]
+      // });
       // this.gaodeMap.on('indoor_create',function(){
       //     that.gaodeMap.indoorMap.showIndoorMap('B000A856LJ',5);
       // })
+      // 方式二创建室内地图，控件形式
+      AMap.plugin(['AMap.IndoorMap', 'AMap.Geocoder'], function() {
+        var indoorMap = new AMap.IndoorMap({alwaysShow:true});
+        //设定在没有矢量底图的时候也显示，默认情况下室内图仅在有矢量底图的时候显示
+        that.gaodeMap = new AMap.Map('mapContainer', {
+            zoom: 18,
+            resizeEnable: false,
+            showIndoorMap:false,//隐藏地图自带的室内地图图层
+            layers:[indoorMap,new AMap.TileLayer()]
+        });
 
-      AMap.plugin(['AMap.ToolBar','AMap.Scale','AMap.OverView','AMap.IndoorMap'],
-      function(){
-        that.gaodeMap.addControl(new AMap.ToolBar());
+        indoorMap.on('click', function(result){
+          // console.log(result)
+          // 新建地址查询
+          let geoCoder = new AMap.Geocoder();
 
-        that.gaodeMap.addControl(new AMap.Scale());
+          // 新建信息窗体
+          let infoWindow = new AMap.InfoWindow();
 
-        that.gaodeMap.addControl(new AMap.OverView({isOpen:true}));
-
-        that.gaodeMap.addControl(new AMap.IndoorMap());
+          let address = '';
+          let lnglat = [result.lnglat.lng, result.lnglat.lat]
+          geoCoder.getAddress(lnglat,function(status,result){
+            if (status === 'complete' && result.info === 'OK') {
+              console.log(result.regeocode)
+              address = result.regeocode.formattedAddress; //返回地址描述
+              infoWindow.setContent(address);
+              infoWindow.open(that.gaodeMap, lnglat);
+            }
+          })
+        })
+        indoorMap.showLabels();
+        indoorMap.showIndoorMap('B000A856LJ',2,"GD0000080210100027"); //building_id, 楼层，shop_id
       });
+    },
+    initIndoor(){
+      var map = new Indoor.Map('mapContainer',{
+        key:'开发者key',
+        buildingId:'建筑物ID'
+      });
+      //由于地图数据使用了异步加载，为避免出错请把所有的逻辑放在mapready事件内
+      map.once('mapready',function(){
+        console.log(map.getFloor());
+      })
     }
   },
 }
