324ae4e15560a22a4f799c3ab230105ea8d208cf
Anluis <aforanluis@gmail.com>
2018-03-06 11:51:42 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,38 +2,50 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
   },
-  env: {
-    browser: true,
-  },
+  // env: {
+  //   browser: true,
+  // },
   extends: 'airbnb-base',
   // required to lint *.vue files
-  plugins: [
-    'html'
+  plugins: ['html'],
+  extends: [
+    'plugin:vue/essential',
+    'plugin:vue/recommended',
+    'plugin:vue/base',
+    'plugin:vue/strongly-recommended',
   ],
   // check if imports actually resolve
-  'settings': {
+  settings: {
     'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
+      webpack: {
+        config: 'build/webpack.base.conf.js',
+      },
+    },
   },
   // add your custom rules here
-  'rules': {
+  rules: {
     // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+    //   'import/extensions': [
+    //     'error',
+    //     'always',
+    //     {
+    //       js: 'never',
+    //       vue: 'never',
+    //     },
+    //   ],
+    //   // allow optionalDependencies
+    //   'import/no-extraneous-dependencies': [
+    //     'error',
+    //     {
+    //       optionalDependencies: ['test/unit/index.js'],
+    //     },
+    //   ],
+    //   // allow debugger during development
+    //   'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  },
 }
