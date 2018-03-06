a3e9494660c7cd158852697a045f7ce1cd60ce33
Anluis <aforanluis@gmail.com>
2018-03-06 18:19:36 +0800

    animation

--- src/pages/fengDemo/demo2.vue
+++ src/pages/fengDemo/demo2.vue
@@ -24,7 +24,7 @@ export default {
       try {
         this.ctlOpt = await this.ctlOptInit()
         await this.initMap()
-        await this.addImageMarker()
+        // await this.addImageMarker()
         this.clickEventInit()
       } catch (e) {
         console.log(e)
@@ -51,6 +51,7 @@ export default {
             this.fMap,
             this.ctlOpt
           )
+          this.addImageMarker()
           resolve()
         })
       })
@@ -72,6 +73,33 @@ export default {
     clickEventInit() {
       this.fMap.on('mapClickNode', e => {
         console.log(e)
+        if (e.nodeType === fengmap.FMNodeType.IMAGE_MARKER) {
+          let group = this.fMap.getFMGroup(this.fMap.groupIDs[1])
+          let layer = group.getOrCreateLayer('imageMarker')
+          let addMarker = new fengmap.FMImageMarker({
+            url: 'static/feng/image/redpack.png',
+            size: 64,
+            x: 13528128.411865031,
+            y: 3662307.8463413287,
+            z: 0,
+            callback: () => {
+              console.log('callback')
+              addMarker.alwaysShow()
+              addMarker.jump({
+                times: 0,
+                duration: 1,
+                dalay: 0.5,
+                height: 1
+              })
+            }
+          })
+
+          // addMarker.x = 13528128.411865031
+          // addMarker.y = 3662307.8463413287
+          // addMarker.z = 0
+          layer.addMarker(addMarker)
+          // console.dir(addMarker.jump)
+        }
       })
     },
     addImageMarker() {
@@ -96,6 +124,12 @@ export default {
           z: 0,
           callback: () => {
             F1_2.alwaysShow()
+            F1_2.jump({
+              times: 0,
+              duration: 1,
+              delay: 0.5,
+              height: 10
+            })
           }
         })
         let F1_3 = new fengmap.FMImageMarker({
@@ -111,9 +145,16 @@ export default {
         layer.addMarker(F1_1)
         layer.addMarker(F1_2)
         layer.addMarker(F1_3)
+        // F1_2.jump({
+        //   times: 0,
+        //   duration: 1,
+        //   delay: 0.5,
+        //   height: 10
+        // })
         resolve()
       })
-    }
+    },
+    handleMarkerClick() {}
   }
 }
 </script>
