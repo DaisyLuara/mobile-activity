a7b06a3fd550dd55c1417d1b9d2b333c37fee7c4
Anluis <aforanluis@gmail.com>
2018-03-14 16:17:41 +0800

    fix

--- src/pages/worldCupPlanet/index.vue
+++ src/pages/worldCupPlanet/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="wcp-wrap" ref="wcpRoot">
-    
   </div>
 </template>
 
@@ -8,8 +7,8 @@
 export default {
   data() {
     return {
-      mH: 400,
-      mW: 400,
+      base: 0,
+      count: 5,
       mData: [
         ['point1', 77],
         ['point2', 72],
@@ -17,19 +16,12 @@ export default {
         ['point4', 50],
         ['point5', 80],
         ['point6', 90]
-      ],
-      mCount: 6,
-      mCenter: 200,
-      mRadius: 100,
-      mAngle: Math.PI * 2 / 6,
-      mCtx: null,
-      mColorPolygon: '#B8B8B8',
-      mColorLines: '#B8B8B8',
-      mColorText: '#000000'
+      ]
     }
   },
   mounted() {
     $('.wcp-wrap').css('height', $(window).height())
+    this.base = $(window).width()
     this.init()
   },
   methods: {
@@ -37,124 +29,53 @@ export default {
       let canvas = document.createElement('canvas')
       let rootDiv = this.$refs['wcpRoot']
       rootDiv.appendChild(canvas)
-      canvas.height = this.mH
-      canvas.width = this.mW
+      canvas.height = this.base
+      canvas.width = this.base
       this.mCtx = canvas.getContext('2d')
 
       this.drawPolygon(this.mCtx)
-      this.drawLines(this.mCtx)
-      this.drawText(this.mCtx)
-      this.drawRegion(this.mCtx)
-      this.drawCircle(this.mCtx)
+      this.drawPoint(this.mCtx)
     },
     drawPolygon(ctx) {
+      let ang = Math.PI * 2 / this.count
+      let r = this.base / 2
       ctx.save()
-
-      ctx.strokeStyle = this.mColorPolygon
-      let r = this.mRadius / this.mCount
-      for (let i = 0; i < this.mCount; i++) {
-        ctx.beginPath()
-        let currR = r * (i + 1)
-        for (let j = 0; j < this.mCount; j++) {
-          let x = this.mCenter + currR * Math.cos(this.mAngle * j)
-          let y = this.mCenter + currR * Math.sin(this.mAngle * j)
-
-          ctx.lineTo(x, y)
-        }
-        ctx.closePath()
-        ctx.stroke()
-      }
-      ctx.restore()
-    },
-    drawLines(ctx) {
-      ctx.save()
-
+      ctx.strokeStyle = '#38FFFD'
+      ctx.lineWidth = 2
+      ctx.translate(r, r)
+      ctx.moveTo(0, -r)
       ctx.beginPath()
-      ctx.strokeStyle = this.mColorLines
-
-      for (let i = 0; i < this.mCount; i++) {
-        let x = this.mCenter + this.mRadius * Math.cos(this.mAngle * i)
-        let y = this.mCenter + this.mRadius * Math.sin(this.mAngle * i)
-        ctx.moveTo(this.mCenter, this.mCenter)
-        ctx.lineTo(x, y)
+      for (let i = 0; i < this.count; i++) {
+        ctx.rotate(ang)
+        ctx.lineTo(0, -r)
       }
+      ctx.closePath()
+      // ctx.fill()
       ctx.stroke()
       ctx.restore()
     },
-    drawText(ctx) {
+    drawPoint(ctx) {
+      let ang = Math.PI * 2 / this.count
+      let r = this.base / 2
       ctx.save()
-
-      let fontSize = this.mCenter / 12
-      ctx.font = fontSize + 'px'
-      ctx.fillStyle = this.mColorText
-
-      for (let i = 0; i < this.mCount; i++) {
-        let x = this.mCenter + this.mRadius * Math.cos(this.mAngle * i)
-        let y = this.mCenter + this.mRadius * Math.sin(this.mAngle * i)
-
-        if (this.mAngle * i >= 0 && this.mAngle * i <= Math.PI / 2) {
-          ctx.fillText(this.mData[i][0], x, y + fontSize)
-        } else if (
-          this.mAngle * i > Math.PI / 2 &&
-          this.mAngle * i <= Math.PI
-        ) {
-          ctx.fillText(
-            this.mData[i][0],
-            x - ctx.measureText(this.mData[i][0]).width,
-            y + fontSize
-          )
-        } else if (
-          this.mAngle * i > Math.PI &&
-          this.mAngle * i <= Math.PI * 3 / 2
-        ) {
-          ctx.fillText(
-            this.mData[i][0],
-            x - ctx.measureText(this.mData[i][0]).width,
-            y
-          )
-        } else {
-          ctx.fillText(this.mData[i][0], x, y)
+      ctx.fillStyle = 'rgba(255,0,0,.3)'
+      ctx.strokeStyle = '#38FFFD'
+      ctx.lineWidth = 1
+      ctx.translate(r, r)
+      ctx.moveTo(0, -r)
+      ctx.beginPath()
+      for (let i = 0; i < this.count; i++) {
+        ctx.rotate(ang)
+        ctx.lineTo(0, -r * this.mData[i][1] / 100)
+        let imgObj = new Image()
+        imgObj.src = '/static/wcp/point.png'
+        imgObj.onload = () => {
+          ctx.drawImage(imgObj, 0, r * this.mData[i][1] / 100)
         }
       }
-
-      ctx.restore()
-    },
-    drawRegion(ctx) {
-      ctx.save()
-
-      ctx.beginPath()
-      for (let i = 0; i < this.mCount; i++) {
-        let x =
-          this.mCenter +
-          this.mRadius * Math.cos(this.mAngle * i) * this.mData[i][1] / 100
-        let y =
-          this.mCenter +
-          this.mRadius * Math.sin(this.mAngle * i) * this.mData[i][1] / 100
-        ctx.lineTo(x, y)
-      }
       ctx.closePath()
-      ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'
-      ctx.fill()
-      ctx.restore()
-    },
-    drawCircle(ctx) {
-      ctx.save()
-
-      let r = this.mCenter / 18
-      for (let i = 0; i < this.mCount; i++) {
-        let x =
-          this.mCenter +
-          this.mRadius * Math.cos(this.mAngle * i) * this.mData[i][1] / 100
-        let y =
-          this.mCenter +
-          this.mRadius * Math.sin(this.mAngle * i) * this.mData[i][1] / 100
-
-        ctx.beginPath()
-        ctx.arc(x, y, r, 0, Math.PI * 2)
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.8)'
-        ctx.fill()
-      }
-
+      // ctx.fill()
+      ctx.stroke()
       ctx.restore()
     }
   }
