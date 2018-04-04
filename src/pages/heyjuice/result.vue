751a0307f18e61ddbb348542bb6e6d0fd9107894
daisyluara <1481795291@qq.com>
2018-04-04 10:46:25 +0800

    修改图片透明度

--- src/pages/heyjuice/result.vue
+++ src/pages/heyjuice/result.vue
@@ -160,12 +160,12 @@ export default {
   //   transform: translateY(0);
   // }
   0% {
-    opacity: 0;
+    // opacity: 0;
     transform: translate3d(0, -100%, 0);
   }
 
   100% {
-    opacity: 1;
+    // opacity: 1;
     transform: none;
   }
 }
