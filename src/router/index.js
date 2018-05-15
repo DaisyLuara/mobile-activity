f2654f452f2490abdcbeb838595796c10a0a0202
jun <Synac1012@aliyun.com>
2018-05-15 15:19:15 +0800

    update  kissMe

--- src/router/index.js
+++ src/router/index.js
@@ -419,6 +419,12 @@ export default new Router({
           name: '星视度玩法宣传视频',
           component: () =>
             import(/* webpackChunkName: "ppt" */ 'pages/ppt/v1.vue')
+        },
+        {
+          path: 'kissMe',
+          name: 'kissMe电影通用版',
+          component: () =>
+            import(/* webpackChunkName: "kissMe" */ 'pages/kissMe/result.vue')
         }
       ]
     },
