d49d5a61c7e5e2745f78438b13bf32e5639debab
Anluis <aforanluis@gmail.com>
2018-05-28 15:03:58 +0800

    update

--- src/pages/pjfd/index.vue
+++ src/pages/pjfd/index.vue
@@ -1,17 +1,67 @@
 <template>
-  <div class="root">
-     <img
+  <div
+    :style="style.root" 
+    class="root">
+
+    <!-- 背景 -->
+    <img
       class="bg"
       :src="IMAGE_URL + 'bg.png'" />
+    
+    <!-- 背景区域 -->
+    <div class="photo-area">
+      <img
+      :class="{'photo-inner-horn': isHorn, 'photo-inner-nothorn': !isHorn}"
+      :src="photo"
+      />
+    </div>
+
+    <!-- 实际背景 -->
+    <img
+      class="photo-real"
+      :src="photo"
+      />
+    
+    <!-- tips -->
+    <img
+      class="remind"
+      :src="IMAGE_URL + 'remind.png'" />
   </div>  
 </template>
 
 <script>
+const wih = window.innerHeight
+import marketService from 'services/marketing'
+import { Toast } from 'mint-ui'
 export default {
   data() {
     return {
       IMAGE_URL:
-        'https://h5-images.oss-cn-shanghai.aliyuncs.com/xingshidu_h5/marketing/pages/pjfd/'
+        'https://h5-images.oss-cn-shanghai.aliyuncs.com/xingshidu_h5/marketing/pages/pjfd/',
+      style: {
+        root: {
+          height: wih + 'px'
+        }
+      },
+      isHorn: false,
+      photo: null
+    }
+  },
+  created() {
+    this.getInfoById()
+  },
+  methods: {
+    getInfoById() {
+      let id = this.$route.query.id
+      marketService
+        .getInfoById(this, id)
+        .then(res => {
+          this.photo = res.image
+          this.isHorn = res.isHorn
+        })
+        .catch(err => {
+          Toast(err)
+        })
     }
   }
 }
@@ -22,9 +72,44 @@ export default {
   width: 100%;
   position: relative;
   overflow: hidden;
+  display: flex;
+  justify-content: center;
+  z-index: 1;
   .bg {
     height: 100%;
     position: relative;
+    z-index: 10;
+  }
+  .photo-area {
+    width: 100%;
+    position: absolute;
+    // border: 1px solid white;
+    height: 50%;
+    top: 30%;
+    z-index: 11;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .photo-inner-horn {
+      width: 90%;
+    }
+    .photo-inner-nothorn {
+      height: 100%;
+    }
+  }
+  .photo-real {
+    width: 100%;
+    position: absolute;
+    height: 50%;
+    top: 30%;
+    z-index: 12;
+    opacity: 0;
+  }
+  .remind {
+    position: absolute;
+    bottom: 10%;
+    width: 30%;
+    z-index: 11;
   }
 }
 </style>
