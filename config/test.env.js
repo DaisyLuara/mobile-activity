13f397be53e2c4a87dd7362cdd8ed912d7f3b060
Anluis <aforanluis@gmail.com>
2018-06-29 09:44:00 +0800

    fix

--- config/test.env.js
+++ config/test.env.js
@@ -1,10 +1,10 @@
 'use strict'
 module.exports = {
-  NODE_ENV: String('test'),
-  DOMAIN: String('.loveqiche.com'),
-  M_URL: String('http://h5.newgls.cn'),
-  WX_API: String('http://sapi.newgls.cn/api'),
-  STORE_API: String('http://store.loveqiche.com'),
-  SAAS_API: String('http://sapi.newgls.cn/api'),
-  IMAGE_SERVER: String('http://h5-images.oss-cn-shanghai.aliyuncs.com')
+  NODE_ENV: JSON.stringify('test'),
+  DOMAIN: JSON.stringify('.loveqiche.com'),
+  M_URL: JSON.stringify('http://h5.newgls.cn'),
+  WX_API: JSON.stringify('http://sapi.newgls.cn/api'),
+  STORE_API: JSON.stringify('http://store.loveqiche.com'),
+  SAAS_API: JSON.stringify('http://sapi.newgls.cn/api'),
+  IMAGE_SERVER: JSON.stringify('http://h5-images.oss-cn-shanghai.aliyuncs.com')
 }
