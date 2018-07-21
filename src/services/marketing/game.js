12f093b9726af3dac43df5e37adc0eb36ce9fdb6
Joy <joywangx@sina.com>
2018-07-21 17:16:12 +0800

    header

--- src/services/marketing/game.js
+++ src/services/marketing/game.js
@@ -2,11 +2,16 @@ import axios from 'axios'
 
 const GAME_URL = process.env.SAAS_API + '/user/'
 const GAME_LIST_URL = process.env.SAAS_API + '/user/'
+const REQ_HEADER = {
+  headers: {
+    withCredentials: true
+  }
+}
 
 const createGame = (params, userId) => {
   return new Promise((resolve, reject) => {
     axios
-      .post(GAME_URL + userId + '/game', params)
+      .post(GAME_URL + userId + '/game', params, REQ_HEADER)
       .then(response => {
         resolve(response.data)
       })
