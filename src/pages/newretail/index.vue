17ce8609181170cd5bd27cab19cba04f85a9f870
Anluis <aforanluis@gmail.com>
2018-07-27 15:17:50 +0800

    fix

--- src/pages/newretail/index.vue
+++ src/pages/newretail/index.vue
@@ -7,7 +7,21 @@
 </template>
 
 <script>
-export default {}
+import { $_wechat, wechatShareTrack } from 'services'
+export default {
+  data() {
+    return {
+      wxShareInfoValue: {
+        title: '场景新媒体 赋能新零售',
+        desc: '掌握全球领先智能科技  突破线下体验四大难点',
+        imgUrl: 'http://cdn.exe666.com/fe/marketing/new/share.png',
+        success: function() {
+          wechatShareTrack()
+        }
+      }
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
