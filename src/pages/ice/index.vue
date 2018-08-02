19468bfc3ff158651402995c3fce6c228a842449
jun <Synac1012@aliyun.com>
2018-08-02 16:00:21 +0800

    update  冰川世界

---
+++ src/pages/ice/index.vue
@@ -0,0 +1,261 @@
+<template>
+  <div
+    :style="style.root" 
+    class="root">
+    <div 
+      v-show="shade"
+      class="shade">
+      <img 
+        v-if="photo !== null" 
+        :src="photo + this.$qiniuCompress()"
+        :class="{'X-photoHide':iphoneX}"
+        class="photo-hide" >
+      <div 
+        v-if="hint"
+        class="hint">
+        <img 
+          :src="baseUrl + 'hint2.png'+ this.$qiniuCompress()"
+          class="hint2" > 
+        <img 
+          :src="baseUrl + 'hint1.png'+ this.$qiniuCompress()"
+          class="hint1" > 
+      </div>
+      <img 
+        v-if="iceShow.iceOne"
+        :src="baseUrl + '1.png'+ this.$qiniuCompress()"
+        class="ice-01" >
+      <img 
+        v-if="iceShow.iceTwo"
+        :src="baseUrl + '2.png'+ this.$qiniuCompress()"
+        class="ice-02" >
+      <img 
+        v-if="iceShow.iceThree"
+        :src="baseUrl + '3.png'+ this.$qiniuCompress()"
+        class="ice-02" >
+    </div>
+    <div 
+      v-show="realContent"
+      class="real-content">
+        <img 
+        :src="baseUrl + 'save.png'+ this.$qiniuCompress()"
+        class="save" >
+        <img 
+        v-if="photo !== null" 
+        :src="photo + this.$qiniuCompress()"
+        :class="{'X-photo':iphoneX}"
+        class="photo" >
+    </div>
+  </div>
+</template>
+
+<script>
+const wih = window.innerHeight
+const wiw = window.innerWidth
+import { $wechat, wechatShareTrack, getInfoById, isInWechat } from 'services'
+import { normalPages } from '../../mixins/normalPages'
+const cdnUrl = process.env.CDN_URL
+
+export default {
+  mixins: [normalPages],
+  data() {
+    return {
+      baseUrl: cdnUrl + '/fe/marketing/img/ice/',
+      style: {
+        root: {
+          height: wih + 'px'
+        }
+      },
+      photo: '',
+      shade: true,
+      realContent: false,
+      hint: true,
+      iceShow: {
+        iceOne: false,
+        iceTwo: false,
+        iceThree: false
+      },
+      touchNumber: 0,
+      iphoneX: false,
+      wxShareInfo: {
+        title: '全天欢唱 买一送二',
+        desc: '购买一小时赠送2小时',
+        imgUrl: cdnUrl + '/fe/marketing/img/ice/icon.jpg',
+        success: () => {
+          wechatShareTrack()
+        }
+      }
+    }
+  },
+  created() {
+    document.addEventListener('touchstart', this.touch, false)
+    document.addEventListener('touchmove', this.touch, false)
+    document.addEventListener('touchend', this.touch, false)
+  },
+  mounted() {
+    let height = wih
+    if (height > 672) {
+      this.iphoneX = true
+    } else {
+      this.iphoneX = false
+    }
+  },
+  methods: {
+    touch(event) {
+      var event = event || window.event
+      switch (event.type) {
+        case 'touchstart':
+          break
+        case 'touchend':
+          this.touchNumber++
+          if (this.touchNumber == 1) {
+            this.iceShow.iceOne = true
+            this.hint = false
+          } else if (this.touchNumber == 2) {
+            this.iceShow.iceTwo = true
+            this.iceShow.iceOne = false
+          } else if (this.touchNumber == 3) {
+            this.iceShow.iceThree = true
+            this.iceShow.iceTwo = false
+          } else {
+            this.iceShow.iceThree = false
+            this.realContent = true
+            this.shade = false
+          }
+          break
+        case 'touchmove':
+          // 执行滑动事件
+          break
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@imageHost: 'http://cdn.exe666.com/fe/marketing/img/ice';
+.root {
+  width: 100%;
+  position: relative;
+  text-align: center;
+  .shade {
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+    background-image: url('@{imageHost}/0.jpg');
+    background-size: 100% 100%;
+    .photo-hide {
+      width: 56%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      user-select: none;
+      transform: translate(-50%, -45%);
+    }
+    .X-photoHide {
+      width: 65%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      user-select: none;
+      transform: translate(-50%, -45%);
+    }
+    .hint {
+      width: 100%;
+      text-align: center;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      .hint2 {
+        width: 30%;
+        display: block;
+        margin: 0 auto;
+        animation: fingerScale 2s ease-out infinite forwards;
+      }
+      img {
+        width: 50%;
+      }
+    }
+    .ice-01 {
+      width: 100%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -55%);
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      user-select: none;
+    }
+    .ice-02 {
+      width: 100%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -55%);
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      user-select: none;
+    }
+    .ice-03 {
+      width: 100%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -55%);
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      user-select: none;
+    }
+  }
+  .real-content {
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+    background-image: url('@{imageHost}/4.jpg');
+    background-size: 100% 100%;
+    .save {
+      width: 60%;
+      position: absolute;
+      left: 50%;
+      top: 14%;
+      transform: translate(-50%, 0%);
+      z-index: 5;
+    }
+    .photo {
+      width: 56%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      user-select: none;
+      transform: translate(-50%, -45%);
+    }
+    .X-photo {
+      width: 65%;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      user-select: none;
+      transform: translate(-50%, -45%);
+    }
+  }
+}
+@keyframes fingerScale {
+  from {
+    transform: scale(1.05, 1.05);
+  }
+  50% {
+    transform: scale(1.2, 1.2);
+  }
+  to {
+    transform: scale(1.05, 1.05);
+  }
+}
+</style>
---
+++ src/router/marketing/sortByDate/2018-08.js
@@ -0,0 +1,14 @@
+const Date201808 = [
+  {
+    path: 'ice',
+    location: 'ice/index',
+    name: 'ice',
+    meta: {
+      title: '冰川世界',
+      author: 'xj1012',
+      desc: '冰川世界H5页面'
+    },
+    isAbandoned: false
+  }
+]
+export default Date201808
