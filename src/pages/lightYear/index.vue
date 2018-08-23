0ec404bb3da6f5ffff6358688d3285c2fc8e5126
jun <Synac1012@aliyun.com>
2018-08-23 17:37:55 +0800

    1111

--- src/pages/lightYear/index.vue
+++ src/pages/lightYear/index.vue
@@ -8,9 +8,9 @@
     <div class="rank">
        <div class="top">
          <div class="head-portrait">
+           <!-- :src="headImgUrl" -->
            <img 
-           
-              :src="baseUrl + 'center.png'+ this.$qiniuCompress()"
+              src="http://cdn.exe666.com/fe/marketing/img/internationalTrade/666.png"
               class="wx-head" >
          </div>
          <p class="num">1,131,223</p>
@@ -22,6 +22,8 @@
             <img 
               :src="baseUrl + 'one.png'+ this.$qiniuCompress()"
               class="wx-head" >
+            <img src="http://cdn.exe666.com/fe/marketing/img/internationalTrade/666.png"
+              class="wx"> 
             <span class="score-num">1,131,223</span>
             <span class="score-tit">光年</span>
            </li>
@@ -29,6 +31,8 @@
             <img 
               :src="baseUrl + 'two.png'+ this.$qiniuCompress()"
               class="wx-head" >
+            <img src="http://cdn.exe666.com/fe/marketing/img/internationalTrade/666.png"
+              class="wx"> 
             <span class="score-num">1,131,223</span>
             <span class="score-tit">光年</span>
            </li>
@@ -36,6 +40,8 @@
             <img 
               :src="baseUrl + 'three.png'+ this.$qiniuCompress()"
               class="wx-head" >
+            <img src="http://cdn.exe666.com/fe/marketing/img/internationalTrade/666.png"
+              class="wx"> 
             <span class="score-num">1,131,223</span>
             <span class="score-tit">光年</span>
            </li>
@@ -57,11 +63,23 @@
          </ul>
        </div>
     </div>
+    <img 
+      :src="baseUrl + 'per.png'+ this.$qiniuCompress()"
+      class="per" >
+    <img 
+      :src="baseUrl + 'rocket.png'+ this.$qiniuCompress()"
+      class="rocket" >
   </div>
 </template>
 
 <script>
-import { $wechat, getInfoById, wechatShareTrack } from 'services'
+import {
+  isInWechat,
+  Cookies,
+  userGame,
+  $wechat,
+  wechatShareTrack
+} from 'services'
 import { normalPages } from '../../mixins/normalPages'
 const cdnUrl = process.env.CDN_URL
 
@@ -76,6 +94,11 @@ export default {
         }
       },
       photo: '',
+      hammerhigh: null,
+      userId: null,
+      score: null,
+      headImgUrl: null,
+      rank_url: process.env.SAAS_API + '/user/',
       wxShareInfoValue: {
         title: '月满中秋 心愿祈福',
         desc: '家人有爱口难开？让星视度帮你把祝福送给你爱的人吧',
@@ -91,13 +114,67 @@ export default {
     }
   },
   created() {},
-  mounted() {},
-  methods: {}
+  mounted() {
+    //微信授权
+    if (isInWechat() === true) {
+      if (
+        process.env.NODE_ENV === 'production' ||
+        process.env.NODE_ENV === 'testing'
+      ) {
+        this.handleWechatAuth()
+      }
+    }
+  },
+  methods: {
+    handleWechatAuth() {
+      if (Cookies.get('user_id') === null) {
+        let base_url = encodeURIComponent(String(window.location.href))
+        let redirct_url =
+          process.env.WX_API +
+          '/wx/officialAccount/oauth?url=' +
+          base_url +
+          '&scope=snsapi_base'
+        window.location.href = redirct_url
+      } else {
+        //获取微信头像
+        getWxUserInfo().then(r => {
+          this.headImgUrl = r.data.headimgurl
+        })
+        this.score = this.$route.query.score
+        this.hammerhigh = this.$route.query.hammerhigh
+        this.userId = Cookies.get('user_id')
+        this.getGradeList(this.userId)
+      }
+    },
+
+    //获取排行榜
+    getGradeList(userId) {
+      let query = '?belong=' + this.hammerhigh + '&score=' + this.score
+      this.$http
+        .get(this.rank_url + userId + '/rank' + query)
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
 @imageHost: 'http://cdn.exe666.com/fe/marketing/img/lightYear/';
+@font-face {
+  font-family: 'MyNewFont'; /*字体名称*/
+  src: url('@{imageHost}ht.ttf'); /*字体源文件*/
+}
+@font-face {
+  font-family: 'MyNewFont';
+  src: url('../font/pinghei.eot');
+  font-weight: normal;
+  font-style: normal;
+}
 .root {
   width: 100%;
   position: relative;
@@ -113,11 +190,23 @@ export default {
     top: 50%;
     transform: translate(-50%, -50%);
   }
+  .per {
+    width: 60%;
+    position: absolute;
+    left: -30%;
+    top: 3%;
+  }
+  .rocket {
+    width: 24%;
+    position: absolute;
+    right: 3%;
+    bottom: 0;
+  }
   .rank {
     width: 80%;
-    height: 80%;
+    height: 82%;
     background-image: url('@{imageHost}kuang.png');
-    background-size: 100% auto;
+    background-size: 100% 100%;
     background-repeat: no-repeat;
     position: absolute;
     left: 50%;
@@ -127,9 +216,9 @@ export default {
       width: 100%;
       height: 40%;
       position: relative;
+      font-family: 'MyNewFont';
       .head-portrait {
-        width: 30%;
-        height: 42%;
+        width: 28%;
         position: absolute;
         left: 50%;
         top: -13%;
@@ -139,7 +228,6 @@ export default {
         border-radius: 50%;
         .wx-head {
           width: 100%;
-          //height: 100%;
           border-radius: 50%;
         }
       }
@@ -148,24 +236,29 @@ export default {
         text-align: center;
         position: absolute;
         left: 50%;
-        top: 27%;
+        top: 7%;
+        font-size: 26px;
         transform: translate(-50%, 15%);
         color: #fcdb65;
+        font-size: 10vw;
       }
       .light-year {
         width: 100%;
         text-align: center;
         position: absolute;
         left: 50%;
-        top: 40%;
+        top: 3%;
         transform: translate(-50%, 0%);
         color: #2ad7e7;
+        font-size: 6.5vw;
       }
     }
     .bottom {
       width: 100%;
       height: 60%;
       position: relative;
+      font-family: 'MyNewFont';
+      font-size: 5vw;
       .score-rank {
         width: 100%;
         position: absolute;
@@ -175,9 +268,18 @@ export default {
           width: 80%;
           color: #fff;
           margin: 5px 0 2px 0;
+          position: relative;
           .wx-head {
             width: 20%;
             margin: 0 5px;
+          }
+          .wx {
+            width: 18%;
+            position: absolute;
+            left: 10%;
+            top: 0;
+            border-radius: 50%;
+            z-index: -1;
           }
           .score-num {
             margin: 0 5px;
