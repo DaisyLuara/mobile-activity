b6974266ff616e63e065e54b218a98a286861412
daisyluara <1481795291@qq.com>
2018-09-03 11:32:27 +0800

    sf

---
+++ src/pages/tm/sfmoon.vue
@@ -0,0 +1,41 @@
+<template>
+  <div 
+    :style="style.root"
+    class="content">
+  </div>
+</template>
+<script>
+import { $wechat, wechatShareTrack } from 'services'
+import { normalPages } from '../../mixins/normalPages'
+const IMG_SERVER = 'http://p22vy0aug.bkt.clouddn.com'
+export default {
+  mixins: [normalPages],
+  data() {
+    return {
+      style: {
+        root: {
+          'min-height': this.$innerHeight() + 'px'
+        }
+      },
+      base: IMG_SERVER + '/image/panda/',
+      photo: null,
+      //分享
+      wxShareInfoValue: {
+        title: '',
+        desc: '',
+        link: '',
+        imgUrl: '',
+        success: function() {
+          wechatShareTrack()
+        }
+      }
+    }
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped>
+</style>
+
+
