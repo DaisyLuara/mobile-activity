4225733c638296f0478276574f3cb6591dcfbc68
Anluis <aforanluis@gmail.com>
2018-09-14 10:15:12 +0800

    add hidol page

---
+++ src/pages/hidol/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hidol-root">
+    <p @click="handleNewPostControl">import lazy async components </p>
+    <new-post v-if="control.newpost"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      control: {
+        newpost: false
+      }
+    }
+  },
+  components: {
+    NewPost: () => import('./components/newPost.vue')
+  },
+  methods: {
+    handleNewPostControl() {
+      this.control.newpost = !this.control.newpost
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.hidol-root {
+  display: flex;
+}
+</style>
