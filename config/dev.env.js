5b22efb85f03b88ac1ed4124a58b9d2eb6422158
Anluis <aforanluis@gmail.com>
2018-09-20 15:43:20 +0800

    delete loveqiche

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: "'development'",
   M_URL: "'http://127.0.0.1:8087'",
   AD_API: "'http://papi.jingfree.top'",
-  STORE_API: "'http://store.loveqiche.com'",
+  STORE_API: "'http://store.vookan.com'",
   SAAS_API: "'http://sapi.jingfree.top/api'",
   WX_API: "'http://sapi.jingfree.top/api'",
   IMAGE_SERVER: "'http://h5-images.oss-cn-shanghai.aliyuncs.com'",
