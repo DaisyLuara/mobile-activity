8ead0a645af367de28ec61665211c4744fd86d70
daisyluara <1481795291@qq.com>
2018-09-30 16:21:10 +0800

    status

--- src/pages/tm/childDream.vue
+++ src/pages/tm/childDream.vue
@@ -280,7 +280,7 @@ export default {
       checkGetCoupon(args)
         .then(res => {
           console.log(res)
-          if (res.statusCode === 200) {
+          if (res.status === 200) {
             this.form = false
           } else {
             this.form = true
