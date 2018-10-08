ee88da1e3cf2fbeb5bc954cde42a85b97f9b7fb2
jun <Synac1012@aliyun.com>
2018-10-08 09:48:56 +0800

    天降美食

---
+++ src/router/marketing/sortByDate/2018-10.js
@@ -0,0 +1,14 @@
+const Date201810 = [
+  {
+    path: 'sjh',
+    location: 'tm/sjh',
+    name: 'sjh',
+    meta: {
+      title: '天降美食',
+      author: 'xj1012',
+      desc: '天降美食'
+    },
+    isAbandoned: false
+  }
+]
+export default Date201810
