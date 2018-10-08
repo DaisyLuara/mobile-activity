5efded1e5253b154d315fd314f6aa06e0c74f005
jun <Synac1012@aliyun.com>
2018-10-08 11:02:57 +0800

    金秋十月

--- src/router/marketing/sortByDate/2018-10.js
+++ src/router/marketing/sortByDate/2018-10.js
@@ -9,6 +9,17 @@ const Date201810 = [
       desc: '天降美食'
     },
     isAbandoned: false
+  },
+  {
+    path: 'autumn_october',
+    location: 'midAutumn/autumnOctober',
+    name: 'autumn_october',
+    meta: {
+      title: '金秋十月',
+      author: 'xj1012',
+      desc: '金秋十月'
+    },
+    isAbandoned: false
   }
 ]
 export default Date201810
