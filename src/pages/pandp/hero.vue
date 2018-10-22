436123cdfc876248e054be053e4e9dcae79f8988
daisyluara <1481795291@qq.com>
2018-10-22 16:13:16 +0800

    复仇毁灭者01

--- src/pages/pandp/hero.vue
+++ src/pages/pandp/hero.vue
@@ -107,16 +107,17 @@ export default {
       note: true,
       photo: null,
       icon: [],
-      rank_score: ['100', '98.6', '97'],
+      rank_score: [],
       params: {
         userId: null,
         id: this.$route.query.id,
         belong: this.$route.query.utm_campaign
       },
       rank_url: process.env.SAAS_API + '/game/rank',
+      user_rank: process.env.SAAS_API + '/user/',
       sign: null,
       data: null,
-      num: 7,
+      num: null,
       //微信分享
       wxShareInfoValue: {
         title: '超级英雄',
@@ -213,6 +214,20 @@ export default {
           this.con[1] = this.data[1].imgUrl
           this.con[2] = this.data[2].imgUrl
           console.log(this.data)
+          this.getRank(this.params.userId)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    getRank(userId) {
+      let query = '?belong=' + this.params.belong + '&score=' + this.score
+      this.$http
+        .get(this.user_rank + userId + '/rank' + query)
+        .then(res => {
+          console.log('res.data.data')
+          console.log(res.data)
+          console.log(res.data.data)
         })
         .catch(err => {
           console.log(err)
@@ -225,9 +240,10 @@ export default {
           type = 'canvas'
         }
         PIXI.utils.sayHello(type)
+
         let play_div = document.getElementById('play')
-        let width = play_div.innerWidth
-        let height = play_div.innerHeight
+        let width = play_div.clientWidth
+        let height = play_div.clientHeight
         let app = new PIXI.Application(width, height, {
           antialias: true,
           transparent: true
@@ -235,35 +251,245 @@ export default {
         play_div.appendChild(app.view)
         app.renderer.autoResize = true
         app.renderer.resize(width, height)
-        app.stop()
+        let bg = imageSprite('/static/hero/bg.png', 0, 0, width, height)
+
         if (this.role == 1) {
-          PIXI.loader
-            .add('spritesheet', 'required/assets/mc.json')
-            .load(function() {
-              var role1 = [],
-                i
-              for (i = 0; i < 26; i++) {
-                var texture = PIXI.Texture.fromFrame(
-                  'Explosion_Sequence_A ' + (i + 1) + '.png'
-                )
-                role1.push(texture)
-              }
-              for (i = 0; i < 50; i++) {
-                // create an explosion AnimatedSprite
-                var explosion = new PIXI.extras.AnimatedSprite(role1)
-
-                explosion.x = Math.random() * app.screen.width
-                explosion.y = Math.random() * app.screen.height
-                explosion.anchor.set(0.5)
-                explosion.rotation = Math.random() * Math.PI
-                explosion.scale.set(0.75 + Math.random() * 0.5)
-                explosion.gotoAndPlay(Math.random() * 27)
-                app.stage.addChild(explosion)
-              }
-
-              app.start()
-            })
+          playRole1()
           return
+        }
+        if (this.role == 2) {
+          playRole2()
+          return
+        }
+        if (this.role == 3) {
+          playRole3()
+          return
+        }
+        if (this.role == 4) {
+          playRole4()
+          return
+        }
+
+        // 动画
+        function doAnimate(number, name, x, y, width, height, speed, start) {
+          let frames = []
+          for (let i = 0; i < number; i++) {
+            let val = i < 10 ? '0' + i : i
+            start === 1 && i === 0
+              ? null
+              : frames.push(PIXI.Texture.fromFrame(name + val + '.png'))
+          }
+          let anim = new PIXI.extras.AnimatedSprite(frames)
+          anim.x = x
+          anim.y = y
+          anim.width = width
+          anim.height = height
+          anim.animationSpeed = speed
+          anim.play()
+          app.stage.addChild(anim)
+          return anim
+        }
+        //设置样式
+        function setAttribute(sprite, width, height, x, y) {
+          sprite.width = width
+          sprite.height = height
+          ;(sprite.x = x), (sprite.y = y)
+        }
+        //循环获取帧
+        function getFrames(number, name, type) {
+          let frames = []
+          for (let i = 0; i < number; i++) {
+            let val = i < 10 ? '0' + i : i
+            frames.push(PIXI.Texture.fromFrame(name + val + type))
+          }
+          return frames
+        }
+        //图片精灵
+        function imageSprite(url, x, y, width, height) {
+          let sprite = PIXI.Sprite.fromImage(url)
+          sprite.x = x
+          sprite.y = y
+          sprite.width = width
+          sprite.height = height
+          app.stage.addChild(sprite)
+          return sprite
+        }
+        //角色1，女机甲
+        function playRole1() {
+          PIXI.loader
+            .add('/static/hero/1/lightning1.json')
+            .add('/static/hero/1/lightning2.json')
+            .add('/static/hero/1/pifeng.json')
+            .load(function(loader, res) {
+              //披风
+              let pifeng = doAnimate(
+                21,
+                'pifeng_00052_000',
+                width * 0.1,
+                height * 0.2,
+                width * 0.8,
+                (450 / 336) * width * 0.8,
+                0.2
+              )
+              let lightning2 = doAnimate(
+                36,
+                'thunder2_2_000',
+                0,
+                0,
+                width,
+                height,
+                0.5
+              )
+              //人
+              let people = imageSprite(
+                '/static/hero/1/people.png',
+                0,
+                0,
+                width,
+                height
+              )
+              //头部
+              let kui = imageSprite(
+                '/static/hero/1/toukui.png',
+                width * 0.35,
+                0,
+                width * 0.4,
+                (163 / 354) * width * 0.4
+              )
+              let head = imageSprite(
+                '/static/hero/1/head.png',
+                width * 0.4,
+                height * 0.075,
+                width * 0.2,
+                (136 / 235) * width * 0.2
+              )
+              let pa = imageSprite(
+                '/static/hero/1/pa.png',
+                width * 0.42,
+                height * 0.13,
+                width * 0.18,
+                (138 / 190) * width * 0.18
+              )
+              let lightning1 = doAnimate(36, '34_000', 0, 0, width, height, 0.5)
+              app.ticker.add(function() {
+                head.y > height * 0.05 ? (head.y -= 0.08) : height * 0.05
+                pa.y < height * 0.2 ? (pa.y += 0.2) : height * 0.2
+              })
+            })
+        }
+        //角色2  雅典娜
+        function playRole2() {
+          PIXI.loader
+            .add('/static/hero/2/Athena.json')
+            .add('/static/hero/2/fires.json')
+            .load(function(loader, res) {
+              //人
+              let people = imageSprite(
+                '/static/hero/2/people.png',
+                0,
+                0,
+                width,
+                height
+              )
+              let fires = doAnimate(
+                24,
+                'FIRE_0',
+                -width * 0.35,
+                -height * 0.35,
+                width,
+                height,
+                0.5
+              )
+              let Athena = doAnimate(
+                24,
+                'Athena_016_000',
+                width * 0.35,
+                -height * 0.1,
+                width * 0.3,
+                (215 / 121) * width * 0.3,
+                0.4,
+                1
+              )
+              Athena.loop = false
+            })
+        }
+        //角色3  钢铁侠
+        function playRole3() {
+          PIXI.loader
+            .add('/static/hero/3/body1.json')
+            .add('/static/hero/3/head.json')
+            .load(function(loader, res) {
+              //人
+              let people = doAnimate(
+                46,
+                'black 2_000',
+                0,
+                0,
+                width,
+                height,
+                0.1,
+                1
+              )
+              // let head1 = doAnimate(
+              //   13,
+              //   'maozi_00168_000',
+              //   width * 0.3,
+              //   height * 0.03,
+              //   width * 0.4,
+              //   (192 / 266) * width * 0.4,
+              //   0.05
+              // )
+              let head2 = doAnimate(
+                20,
+                'man_053_00002_000',
+                width * 0.41,
+                height * 0.02,
+                width * 0.2,
+                (113 / 100) * width * 0.2,
+                0.2
+              )
+              head2.loop = false
+              people.loop = false
+              // head1.loop = false
+            })
+        }
+        //角色4  超人
+        function playRole4() {
+          PIXI.loader
+            .add('/static/hero/4/piao.json')
+            .load(function(loader, res) {
+              let pao = doAnimate(
+                32,
+                'piao_000',
+                0,
+                height * 0.12,
+                width,
+                (293 / 226) * width,
+                0.5
+              )
+              //人
+              let people = imageSprite(
+                '/static/hero/4/people.png',
+                (width - (284 / 846) * height) / 2,
+                0,
+                (284 / 846) * height,
+                height
+              )
+              // let sao = imageSprite(
+              //   '/static/hero/4/sao.png',
+              //   0,
+              //   0,
+              //   width,
+              //   height
+              // )
+              let light = imageSprite(
+                '/static/hero/4/light.png',
+                0,
+                0,
+                width,
+                height
+              )
+            })
         }
       })
     }
@@ -340,10 +566,13 @@ img {
         position: absolute;
         right: 0.5%;
         top: 15.5%;
-        border: solid 1px red;
         width: 65%;
         height: 82%;
         z-index: 99;
+        canvas {
+          width: 100%;
+          height: 100%;
+        }
       }
       .text {
         position: absolute;
