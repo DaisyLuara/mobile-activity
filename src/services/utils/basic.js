bc856b8241d2efc2d34b48f89ed7d47f80ef48c8
jun <Synac1012@aliyun.com>
2018-11-15 13:46:15 +0800

    兰花城bug

--- src/services/utils/basic.js
+++ src/services/utils/basic.js
@@ -13,5 +13,29 @@ const validatePhone = number => {
     return false
   }
 }
+//转换日期格式
+const dataFormat = (date, fmt) => {
+  var o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds(),
+    'q+': Math.floor((date.getMonth() + 3) / 3),
+    S: date.getMilliseconds()
+  }
+  if (/(y+)/.test(fmt))
+    fmt = fmt.replace(
+      RegExp.$1,
+      (date.getFullYear() + '').substr(4 - RegExp.$1.length)
+    )
+  for (var k in o)
+    if (new RegExp('(' + k + ')').test(fmt))
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length)
+      )
+  return fmt
+}
 
-export { isInWechat, randomIntNum, validatePhone }
+export { isInWechat, randomIntNum, validatePhone, dataFormat }
