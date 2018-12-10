87bed5413f87a073fa54a9248a5d4fd2042bea78
jun <Synac1012@aliyun.com>
2018-12-10 13:56:50 +0800

    圣诞

---
+++ src/pages/201812/heaven.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    :style="style.root"
+    class="root"
+  >
+
+  </div>
+</template>
+<script>
+import { normalPages } from "../../mixins/normalPages";
+const cdnUrl = process.env.CDN_URL;
+export default {
+  mixins: [normalPages],
+  data() {
+    return {
+      baseUrl: cdnUrl + "/fe/marketing/img/heaven/",
+      style: {
+        root: {
+          height: this.$innerHeight() + "px"
+        }
+      },
+      photo: null,
+      iphoneX: false,
+      wxShareInfoValue: {
+        title: "刷脸制作圣诞贺卡，送给你爱的TA",
+        desc: "这个圣诞节，玩点不一样~",
+        link: "http://papi.xingstation.com/api/s/0YX" + window.location.search,
+        imgUrl: cdnUrl + "/fe/marketing/img/heaven/icon.jpg",
+        success: () => {
+          wechatShareTrack();
+        }
+      }
+    };
+  },
+  mounted() {
+    if (this.$innerHeight() > 672) {
+      this.iphoneX = true;
+    } else {
+      this.iphoneX = false;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@imageHost: "http://cdn.exe666.com/fe/marketing/img/heaven/";
+html,
+body {
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  -webkit-overflow-scrolling: touch;
+  transform: translate3d(0, 0, 0);
+}
+* {
+  padding: 0;
+  margin: 0;
+  font-size: 0;
+  text-align: center;
+}
+img {
+  pointer-events: none;
+  user-select: none;
+  max-width: 100%;
+}
+.root {
+  width: 100%;
+  text-align: center;
+  position: relative;
+  overflow: hidden;
+  background: #2c0206;
+}
+</style>
