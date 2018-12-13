55932db4c62c5cf5897ed012820bc8b69990b17c
Joy <joy@fdvdeMacBook-Pro.local>
2018-12-13 10:45:37 +0800

    fix

--- src/pages/201812/longhuyanzhi.vue
+++ src/pages/201812/longhuyanzhi.vue
@@ -121,7 +121,7 @@ export default {
       };
       getMallcooOauth(args)
         .then(res => {
-          let data = res.data;
+          let data = res;
           window.location.href = data;
           return;
         })
