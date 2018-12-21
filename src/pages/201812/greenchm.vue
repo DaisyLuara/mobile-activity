993cb3b2cfc4c49bdf188e21a9748d951c8d2276
daisyluara <1481795291@qq.com>
2018-12-21 18:16:51 +0800

    remove

--- src/pages/201812/greenchm.vue
+++ src/pages/201812/greenchm.vue
@@ -134,7 +134,7 @@ export default {
       checkCouponNumber(this.parms.coupon_batch_id)
         .then(res => {
           this.imgUrl = res.image_url;
-          this.handleData(res)
+          // this.handleData(res)
           this.checkGetCoupon()
         })
         .catch(err => {
