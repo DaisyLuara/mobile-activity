358d328a5f5216e6df5a78c064e02c8c1a5642b0
daisyluara <1481795291@qq.com>
2018-12-26 11:23:01 +0800

    add address

--- src/pages/201812/couponRain.vue
+++ src/pages/201812/couponRain.vue
@@ -23,9 +23,76 @@ export default {
       }
     };
   },
+  mounted() {
+    this.doAnimate()
+  },
   methods: {
     doAnimate() {
-      import('pixi.js').then(PIXI => { })
+      import('pixi.js').then(PIXI => {
+        let app = new PIXI.Application({
+          width: window.innerWidth,
+          height: window.innerHeight
+        });
+        let content = document.querySelector('.content')
+        content.appendChild(app.view);
+
+        // holder to store the aliens
+        let url = CDNURL + '/fe/image/couponrain/'
+        let aliens = [];
+
+        let totalDudes = 20;
+
+        for (let i = 0; i < totalDudes; i++) {
+          let dude = PIXI.Sprite.fromImage(url + 'th.jpg');
+
+          dude.anchor.set(0.5);
+
+          dude.scale.set(0.2 + Math.random() * 0.3);
+          dude.x = Math.random() * app.screen.width;
+          dude.y = Math.random() * app.screen.height;
+
+          // dude.tint = Math.random() * 0xFFFFFF;
+          dude.direction = Math.random() * Math.PI * 2;
+
+          dude.turningSpeed = Math.random() - 0.8;
+          dude.speed = 2 + Math.random() * 2;
+          aliens.push(dude);
+
+          app.stage.addChild(dude);
+        }
+
+        let dudeBoundsPadding = 100;
+        let dudeBounds = new PIXI.Rectangle(-dudeBoundsPadding,
+          -dudeBoundsPadding,
+          app.screen.width + dudeBoundsPadding * 2,
+          app.screen.height + dudeBoundsPadding * 2);
+
+        app.ticker.add(function () {
+          for (let i = 0; i < aliens.length; i++) {
+
+            let dude = aliens[i];
+            dude.direction += dude.turningSpeed * 0.01;
+            dude.x += Math.sin(dude.direction) * dude.speed;
+            dude.y += Math.cos(dude.direction) * dude.speed;
+            dude.rotation = -dude.direction - Math.PI / 2;
+
+            if (dude.x < dudeBounds.x) {
+              dude.x += dudeBounds.width;
+            }
+            else if (dude.x > dudeBounds.x + dudeBounds.width) {
+              dude.x -= dudeBounds.width;
+            }
+
+            if (dude.y < dudeBounds.y) {
+              dude.y += dudeBounds.height;
+            }
+            else if (dude.y > dudeBounds.y + dudeBounds.height) {
+              dude.y -= dudeBounds.height;
+            }
+          }
+        });
+
+      })
     }
   }
 };
