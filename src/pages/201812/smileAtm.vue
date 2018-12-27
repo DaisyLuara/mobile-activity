eba44833238b201c6f95e1096f2ae1e42d8167ba
jun <Synac1012@aliyun.com>
2018-12-27 17:35:36 +0800

    更改测试环境的券ID

--- src/pages/201812/smileAtm.vue
+++ src/pages/201812/smileAtm.vue
@@ -51,9 +51,10 @@ export default {
       photo: null,
       iphoneX: false,
       // coupon_batch_id: this.$route.query.coupon_batch_id,
-      id: this.$route.query.id,
       // oid: this.$route.query.utm_source,
-      couponID: ['111', '112', '113', '114'],
+      id: this.$route.query.id,
+      //couponID: ['111', '112', '113', '114'],
+      couponID: ['28', '29'],
       new_coupon_batch_id: null,
       qrcodeImg: null,
       hasUsed: false,
@@ -63,8 +64,8 @@ export default {
       wxShareInfoValue: {
         title: "笑容还能换钱花？测一测你的笑容值多少钱。",
         desc: "错过今天，再等N年。",
-        link: "http://papi.xingstation.com/api/s/3QQ" + window.location.search,
-        //link: "http://papi.newgls.cn/api/s/JZo" + window.location.search,
+        //link: "http://papi.xingstation.com/api/s/3QQ" + window.location.search,
+        link: "http://papi.newgls.cn/api/s/JZo" + window.location.search,
         imgUrl: cdnUrl + "/fe/marketing/img/simle_atm/icon.jpg",
         success: () => {
           wechatShareTrack();
