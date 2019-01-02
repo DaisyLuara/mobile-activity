2dcbdb4f9188abf22b99e7ad4b9af87ce42abb27
jun <Synac1012@aliyun.com>
2019-01-02 11:02:52 +0800

    太和 兰花城 老虎机时间转换修改

--- src/pages/201812/tiger.vue
+++ src/pages/201812/tiger.vue
@@ -51,6 +51,7 @@ import {
   formatTimestamp
 } from 'services'
 const cdnUrl = process.env.CDN_URL
+const dayjs = require('dayjs')
 export default {
   mixins: [onlyGetPhoto],
   data() {
@@ -167,14 +168,16 @@ export default {
               coupon_batch_id: this.coupon_batch_id,
               include: 'couponBatch'
             }
-            args.start_date = dateFormat(
-              new Date(formatTimestamp(data, true)),
-              'yyyy-MM-dd hh:mm:ss'
-            )
-            args.end_date = dateFormat(
-              new Date(formatTimestamp(data, false) - 1000),
-              'yyyy-MM-dd hh:mm:ss'
-            )
+            // args.start_date = dateFormat(
+            //   new Date(formatTimestamp(data, true)),
+            //   'yyyy-MM-dd hh:mm:ss'
+            // )
+            // args.end_date = dateFormat(
+            //   new Date(formatTimestamp(data, false) - 1000),
+            //   'yyyy-MM-dd hh:mm:ss'
+            // )
+            args.start_date = dayjs(new Date(formatTimestamp(data, true))).format('YYYY-MM-DD HH:mm:ss')
+            args.end_date = dayjs(new Date(formatTimestamp(data, false) - 1000)).format('YYYY-MM-DD HH:mm:ss')
             checkGetCoupon(args)
               .then(res => {
                 if (res) {
