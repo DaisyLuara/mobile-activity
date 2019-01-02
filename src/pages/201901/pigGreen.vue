e4ef486dfdbe0ec9e523fe2a24d2b292f23d6a71
jun <Synac1012@aliyun.com>
2019-01-02 11:37:21 +0800

    201901

---
+++ src/pages/201901/pigGreen.vue
@@ -0,0 +1,83 @@
+<template>
+  <div
+    :style="style.root"
+    class="root"
+  >
+
+  </div>
+</template>
+<script>
+import { normalPages } from "@/mixins/normalPages";
+const cdnUrl = process.env.CDN_URL;
+export default {
+  mixins: [normalPages],
+  data() {
+    return {
+      baseUrl: cdnUrl + "/fe/marketing/img/pig_green/",
+      style: {
+        root: {
+          height: this.$innerHeight() + "px"
+        }
+      },
+      iphoneX: false,
+      wxShareInfoValue: {
+        title: "猪年来缤纷城福气满满！",
+        desc: "点击领取福气美照 ",
+        link: "http://papi.xingstation.com/api/s/lx6" + window.location.search,
+        imgUrl: cdnUrl + "/fe/marketing/img/pig_green/icon.png",
+      }
+    };
+  },
+  mounted() {
+    if (this.$innerHeight() > 672) {
+      this.iphoneX = true;
+    } else {
+      this.iphoneX = false;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@imageHost: "http://cdn.exe666.com/fe/marketing/img/pig_green/";
+html,
+body {
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  -webkit-overflow-scrolling: touch;
+  transform: translate3d(0, 0, 0);
+}
+* {
+  padding: 0;
+  margin: 0;
+  font-size: 0;
+  text-align: center;
+}
+img {
+  pointer-events: none;
+  user-select: none;
+  max-width: 100%;
+}
+.root {
+  width: 100%;
+  text-align: center;
+  position: relative;
+  overflow: hidden;
+  background-image: url("@{imageHost}bg2.png");
+  background-size: 100% 100%;
+  background-position: center bottom;
+  background-repeat: no-repeat;
+}
+@keyframes arrow {
+  0% {
+    transform: translateY(-5px);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(5px);
+  }
+}
+</style>
---
+++ src/router/marketing/sortByDate/2019-01.js
@@ -0,0 +1,14 @@
+const Date201901 = [
+  {
+    path: 'pig_green',
+    location: '201901/pigGreen',
+    name: 'pig_green',
+    meta: {
+      title: '猪年送福',
+      author: 'xj1012',
+      desc: '周浦绿地 2019新年导视'
+    },
+    isAbandoned: false
+  }
+]
+export default Date201901
