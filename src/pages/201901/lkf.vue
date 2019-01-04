51a9d784c2d0c5acfee0d1ed44fd3474fc7af374
daisyluara <1481795291@qq.com>
2019-01-04 14:22:01 +0800

    连卡福

---
+++ src/pages/201901/lkf.vue
@@ -0,0 +1,220 @@
+<template>
+  <div
+    :style="style.root"
+    class="warp"
+  >
+    <img
+      :src="base + '1.png'"
+      class="right"
+    >
+    <div class="main">
+      <img
+        :src="coupon_img + this.$qiniuCompress()"
+        class="coupon"
+      >
+      <div class="erweima">
+        <img
+          :src="base + '3.png'"
+          class="erbg"
+        >
+        <img
+          :src="qrcodeImg"
+          class="qrcode"
+        >
+        <img
+          v-show="used"
+          :src="base + 'used.png'"
+          class="used"
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { $wechat, isInWechat, wechatShareTrack, Cookies, sendCoupon, checkGetCoupon } from 'services'
+import { normalPages } from '@/mixins/normalPages'
+const CDN_URL = process.env.CDN_URL
+export default {
+  data() {
+    return {
+      base: CDN_URL + '/fe/image/couponrain/lkf/',
+      style: {
+        root: {
+          'min-height': this.$innerHeight() + 'px'
+        }
+      },
+      id: this.$route.query.id,
+      userId: null,
+      coupon_img: null,//'https://cdn.exe666.com/fe/image/couponrain/lkf/Lee.png',
+      qrcodeImg: null,// 'https://cdn.exe666.com/fe/image/couponrain/5c22f3d46c008.png',
+      used: false,
+      //微信分享
+      wxShareInfoValue: {
+        title: "猪福港味年，红包抢翻天！  ",
+        desc: "点击马上抢【连卡福】新春红包！",
+        link: "http://papi.xingstation.com/api/s/K8r" + window.location.search,
+        imgUrl: CDN_URL + "/fe/image/couponrain/share.jpg",
+      }
+    }
+  },
+  watch: {
+    parms() {
+      this.checkGetCoupon()
+    }
+  },
+  mounted() {
+    //微信授权
+    if (isInWechat() === true) {
+      if (
+        process.env.NODE_ENV === 'production' ||
+        process.env.NODE_ENV === 'testing'
+      ) {
+        this.handleWechatAuth()
+      }
+    }
+  },
+  methods: {
+    //微信静默授权
+    handleWechatAuth() {
+      if (Cookies.get('sign') === null) {
+        let base_url = encodeURIComponent(String(window.location.href))
+        let redirct_url =
+          process.env.WX_API +
+          '/wx/officialAccount/oauth?url=' +
+          base_url +
+          '&scope=snsapi_base'
+        window.location.href = redirct_url
+      } else {
+        this.userId = Cookies.get('user_id')
+      }
+    },
+    //判断是否领过优惠券
+    checkGetCoupon() {
+      let args = {
+        coupon_batch_id: this.$route.query.coupon_batch_id,
+        include: 'couponBatch',
+        qiniu_id: this.id
+      }
+      checkGetCoupon(args).then(res => {
+        console.log(res)
+        if (res) {
+          this.handleData(res)
+        } else {
+          this.sendCoupon()
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    //发优惠券
+    sendCoupon() {
+      let args = {
+        include: 'couponBatch',
+        qiniu_id: this.id,
+        oid: this.oid,
+        belong: this.belong
+      }
+      sendCoupon(args, this.$route.query.coupon_batch_id)
+        .then(res => {
+          console.log('sendCoupon', res)
+          this.handleData(res)
+        })
+        .catch(err => {
+          alert(err.response.data.message)
+        })
+    },
+    //处理返回数据
+    handleData(res) {
+      this.qrcodeImg = res.qrcode_url
+      this.coupon_img = res.couponBatch.image_url
+      if (parseInt(res.status) === 1) {
+        this.used = true
+      }
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+@img: "http://cdn.exe666.com/fe/image/couponrain/lkf/";
+html,
+body {
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  -webkit-overflow-scrolling: touch;
+  transform: translate3d(0, 0, 0);
+}
+* {
+  padding: 0;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 0;
+}
+img {
+  max-width: 100%;
+  pointer-events: none;
+  user-select: none;
+}
+.warp {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  overflow-x: hidden;
+  background-image: url("@{img}2.jpg");
+  background-position: center top;
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-color: #c71429;
+  .right {
+    width: 21.5%;
+    position: absolute;
+    top: 25%;
+    right: 1%;
+    z-index: 99;
+  }
+  .main {
+    width: 72%;
+    position: relative;
+    z-index: 0;
+    margin-top: 32%;
+    margin-left: 20%;
+    margin-bottom: 5%;
+    .coupon {
+      position: relative;
+      z-index: 0;
+    }
+    .erweima {
+      position: absolute;
+      top: 55%;
+      left: 43%;
+      transform: translateX(-50%);
+      width: 40vw;
+      .erbg {
+        position: relative;
+        z-index: 0;
+      }
+      .qrcode {
+        width: 23vw;
+        // height: 25vw;
+        position: absolute;
+        top: 11%;
+        left: 50%;
+        z-index: 9;
+        transform: translateX(-50%);
+      }
+      .used {
+        width: 27vw;
+        position: absolute;
+        top: 18%;
+        left: 50%;
+        transform: translateX(-50%);
+        z-index: 999;
+      }
+    }
+  }
+}
+</style>
+
+
--- src/router/marketing/sortByDate/2019-01.js
+++ src/router/marketing/sortByDate/2019-01.js
@@ -20,6 +20,17 @@ const Date201901 = [
       desc: '刷脸抢福袋'
     },
     isAbandoned: false
+  },
+  {
+    path: 'lkf',
+    location: '201901/lkf',
+    name: 'lkf',
+    meta: {
+      title: '连卡福红包雨',
+      author: 'lipan',
+      desc: '连卡福红包雨-大屏h5'
+    },
+    isAbandoned: false
   }
 ]
 export default Date201901
