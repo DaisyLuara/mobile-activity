3d2859cfc84744012140d775e73c31d12a9c16c8
Anluis <aforanluis@gmail.com>
2019-01-07 21:47:48 +0800

    update

---
+++ src/pages/m/components/My/Barrage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="barrage"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.barrage {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
