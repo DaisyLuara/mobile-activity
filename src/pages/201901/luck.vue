9c3bb2de07f4da75edd91672f79e543e1b1fb6a5
jun <Synac1012@aliyun.com>
2019-01-09 14:08:35 +0800

    新年运势

--- src/pages/201901/luck.vue
+++ src/pages/201901/luck.vue
@@ -7,12 +7,38 @@
       :src="baseUrl + 'top.png'+ this.$qiniuCompress()"
       class="topImg"
     >
+    <img
+      :src="baseUrl + 'lantern_left.png'+ this.$qiniuCompress()"
+      class="lantern-l animated linear infinite swing"
+    >
+    <img
+      :src="baseUrl + 'lantern_right.png'+ this.$qiniuCompress()"
+      class="lantern-r animated linear infinite swing"
+    >
+    <img
+      :src="baseUrl + 'blessing_left.png'+ this.$qiniuCompress()"
+      class="blessing-l"
+    >
+    <img
+      :src="baseUrl + 'blessing_right.png'+ this.$qiniuCompress()"
+      class="blessing-r"
+    >
     <div class="center">
-      <!-- <img
+      <img
+        :src="baseUrl + 'frame.png'+ this.$qiniuCompress()"
+        class="frame"
+      >
+      <img
         v-if="photo !== null"
         :src="photo + this.$qiniuCompress()"
         class="photo"
-      > -->
+      >
+      <img
+        v-if="photo !== null"
+        :src="photo + this.$qiniuCompress()"
+        class="real-photo"
+      >
+
     </div>
     <div class="bt">
       <img
@@ -32,12 +58,12 @@
         class="right"
       >
     </div>
-
   </div>
 </template>
 <script>
 import { normalPages } from "@/mixins/normalPages";
 const cdnUrl = process.env.CDN_URL;
+import 'animate.css'
 export default {
   mixins: [normalPages],
   data() {
@@ -48,12 +74,11 @@ export default {
           height: this.$innerHeight() + "px"
         }
       },
-      iphoneX: false,
       wxShareInfoValue: {
-        title: "亲爱的，新年快乐!",
-        desc: "猪年大吉，猪事顺利",
-        link: "http://papi.xingstation.com/api/s/RlL" + window.location.search,
-        imgUrl: cdnUrl + "/fe/marketing/img/new_luck/icon.png"
+        title: "新年运势",
+        desc: "2019锦鲤非你莫属！",
+        link: "http://papi.xingstation.com/api/s/462" + window.location.search,
+        imgUrl: cdnUrl + "/fe/marketing/img/new_luck/icon.jpg"
       }
     };
   }
@@ -84,7 +109,7 @@ img {
 .root {
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: flex-start;
   align-items: center;
   position: relative;
   overflow: hidden;
@@ -99,46 +124,114 @@ img {
     top: 0;
     z-index: 9;
   }
-  // .center {
-  //   width: 78%;
-  //   height: 85%;
-  //   background: #f9f1dc;
-  //   position: relative;
-
-  //   .photo {
-  //     width: 85%;
-  //     position: absolute;
-  //     left: 50%;
-  //     top: 50%;
-  //     transform: translate(-50%, -50%);
-  //   }
-  // }
+  .lantern-l {
+    width: 20%;
+    position: absolute;
+    left: -6%;
+    top: 10%;
+    z-index: 9;
+    animation-duration: 8s;
+  }
+  .lantern-r {
+    width: 20%;
+    position: absolute;
+    right: -6%;
+    top: 10%;
+    z-index: 9;
+    animation-duration: 8s;
+  }
+  .blessing-l {
+    width: 16%;
+    position: absolute;
+    left: 0%;
+    top: 50%;
+    transform: translate(0, -50%);
+    z-index: 9;
+  }
+  .blessing-r {
+    width: 16%;
+    position: absolute;
+    right: 0%;
+    top: 50%;
+    transform: translate(0, -50%);
+    z-index: 9;
+  }
+  .center {
+    width: 80%;
+    margin-top: 15%;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    overflow: hidden;
+    pointer-events: none;
+    user-select: none;
+    .frame {
+      width: 100%;
+    }
+    .photo {
+      width: 80%;
+      position: absolute;
+      left: 50%;
+      top: 0%;
+      transform: translate(-50%, 0%);
+      margin-top: 10%;
+    }
+    .real-photo {
+      width: 80%;
+      position: absolute;
+      left: 50%;
+      top: 0%;
+      transform: translate(-50%, 0%);
+      margin-top: 10%;
+      opacity: 0;
+      z-index: 99;
+      pointer-events: auto;
+      user-select: auto;
+    }
+  }
   .bt {
     position: absolute;
     left: 0;
     bottom: 0;
     z-index: 9;
+    pointer-events: none;
+    user-select: none;
     .btImg {
       width: 100%;
     }
     .nav {
       width: 22%;
       position: absolute;
-      left: 50%;
+      left: 39%;
       top: -6%;
-      transform: translate(-50%, 0);
+      animation: arrow 0.5s linear infinite alternate;
     }
     .left {
       width: 27%;
       position: absolute;
       left: 15%;
       top: 9%;
+      animation-name: hammerLeft;
+      animation-duration: 0.5s;
+      animation-timing-function: linear;
+      animation-iteration-count: infinite;
+      animation-fill-mode: forwards;
+      animation-direction: alternate;
     }
     .right {
-      width: 27%;
+      width: 26%;
       position: absolute;
-      right: 15%;
+      right: 16%;
       top: 5%;
+      animation-name: hammerRight;
+      animation-duration: 0.5s;
+      animation-timing-function: linear;
+      animation-delay: 0.3s;
+      animation-iteration-count: infinite;
+      animation-fill-mode: forwards;
+      animation-direction: alternate;
     }
   }
 }
@@ -151,6 +244,28 @@ img {
   }
   100% {
     transform: translateY(5px);
+  }
+}
+@keyframes hammerLeft {
+  0% {
+    transform: translateY(-10px);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(4px);
+  }
+}
+@keyframes hammerRight {
+  0% {
+    transform: translateY(-10px);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(4px);
   }
 }
 </style>
