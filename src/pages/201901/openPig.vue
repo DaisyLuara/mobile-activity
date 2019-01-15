5325ab05064ac927042f8afcfd12a6c56533a02d
jun <Synac1012@aliyun.com>
2019-01-15 17:50:46 +0800

    猪年发大财终版

--- src/pages/201901/openPig.vue
+++ src/pages/201901/openPig.vue
@@ -67,7 +67,6 @@ export default {
     }
   },
   mounted() {
-    this.drawing()
     console.log(this.base64Data)
   },
   methods: {
@@ -121,10 +120,8 @@ export default {
       // let url =
       //   'https://cdn.exe666.com/fe/marketing/img/open_pig/666.png'
       let imgUrl = 'https://cdn.exe666.com/fe/marketing/img/open_pig/' + this.fanpaths[this.type]
-      console.log(imgUrl)
       let imgUrl2 = 'https://cdn.exe666.com/fe/marketing/img/open_pig/t.png'
       let imgUrl3 = 'https://cdn.exe666.com/fe/marketing/img/open_pig/' + this.rnd(1, this.paths.length) + '.png'
-      console.log(imgUrl3)
       mc.background('https://cdn.exe666.com/fe/marketing/img/open_pig/bg3.png', {
         left: '0%',
         top: '0%',
