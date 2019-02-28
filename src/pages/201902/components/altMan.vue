36478457c54824dcff0a49fc855db471b2047c06
daisyluara <1481795291@qq.com>
2019-02-28 19:59:05 +0800

    delete mounted

--- src/pages/201902/components/altMan.vue
+++ src/pages/201902/components/altMan.vue
@@ -189,11 +189,12 @@ export default {
     };
   },
   watch: {
-    userinfo() {
+    belong() {
       if (localStorage.getItem('z')) {
-        return
-      }
-      if (this.userinfo.z) {
+        this.z = this.z ? this.z : localStorage.getItem('z')
+        this.projects.total = 3
+        this.$refs.gameHonour.getGameHonour(this.bid, this.z)
+      } else if (this.userinfo.z) {
         this.z = this.userinfo.z
         localStorage.setItem('z', this.userinfo.z)
         this.projects.total = 3
@@ -203,20 +204,6 @@ export default {
   },
   mounted() {
     // this.$refs.gameHonour.getGameHonour(this.bid, '1808ce6f291cc2aa1c33e80d7bbd91128359w5');
-    // if (isiOS) {
-    //   if (localStorage.getItem('altman' + this.id)) {
-    //     return
-    //   } else {
-    //     localStorage.setItem('altman' + this.id, 'share')
-    //     window.location.href = window.location.href
-    //   }
-    // }
-    if (localStorage.getItem('z')) {
-      this.z = this.z ? this.z : localStorage.getItem('z')
-      this.projects.total = 3
-      this.$refs.gameHonour.getGameHonour(this.bid, this.z)
-    }
-
   },
   methods: {
     getCoupon() {
