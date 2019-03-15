858c40033cc0d4e217e5589ce0e7cc11fb815070
daisyluara <1481795291@qq.com>
2019-03-15 15:49:56 +0800

    share

--- src/pages/201903/luckyYu.vue
+++ src/pages/201903/luckyYu.vue
@@ -54,7 +54,7 @@ export default {
       //微信分享
       wxShareInfoValue: {
         title: "幸运锦鲤",
-        desc: "幸运锦鲤",
+        desc: "开启你的锦鲤之旅",
         link: 'http://papi.xingstation.com/api/s/j0z' + window.location.search,
         imgUrl: "https://cdn.exe666.com/fe/image/luckyyu/icon.png"
       }
