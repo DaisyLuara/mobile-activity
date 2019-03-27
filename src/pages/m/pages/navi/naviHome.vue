14aa5c83418b81aa9da57f31cd1aa74bd0599853
Anluis <anluis233@gmail.com>
2019-03-27 18:23:28 +0800

    add navi page

---
+++ src/pages/m/pages/navi/naviHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="navi-root"></div>
+</template>
+
+<script>
+export default {
+  created() {
+    if (this.$route.query.rp) {
+      window.location.href = "http://h5.xingstation.com/m/d7mz317i/6498/wallet";
+    } else {
+      window.location.href =
+        "http://h5.xingstation.com/m/d7mz317i/6498/barrage/index";
+    }
+  }
+};
+</script>
