17b7130eb6cd06b7d152e4fb775a136b903075cb
daisyluara <1481795291@qq.com>
2019-03-27 17:54:06 +0800

    更换分享文本

--- src/pages/201903/dressMC3.vue
+++ src/pages/201903/dressMC3.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <SummerDress
+      ref="summer"
       :link-data="link"
       :bg-data="bg"
     />
@@ -19,7 +20,11 @@ export default {
       bg: CDN_URL + '/fe/image/summer_dress/logo.jpg',
       link: 'http://papi.xingstation.com/api/s/zX7' + window.location.search,
     }
+  },
+  mounted() {
+    this.$refs.summer.wxShareInfoValue.desc = '来吾悦广场，秀“舞”姿嗨翻全场！'
   }
+
 }
 </script>
 <style lang="less" scoped>
