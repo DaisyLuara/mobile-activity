83ae91f68a92fab37188ba3a9baf2b0b273fb369
Anluis <anluis233@gmail.com>
2019-03-28 13:39:07 +0800

    fix params

--- src/pages/m/pages/navi/naviHome.vue
+++ src/pages/m/pages/navi/naviHome.vue
@@ -11,10 +11,12 @@ export default {
       return;
     } else {
       getInfoById(id).then(r => {
-        const params = splitParms(r.parms);
-        if (params.rp) {
-          window.location.href =
-            "http://h5.xingstation.com/m/d7mz317i/6498/wallet";
+        if (r.parms) {
+          const params = splitParms(r.parms);
+          if (params.rp) {
+            window.location.href =
+              "http://h5.xingstation.com/m/d7mz317i/6498/wallet";
+          }
         } else {
           window.location.href =
             "http://h5.xingstation.com/m/d7mz317i/6498/barrage/index";
