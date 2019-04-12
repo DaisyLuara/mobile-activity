92638202cf54064d2fbdc7c7814b521142825d2c
Anluis <zhengwending@jingfree.com>
2019-04-12 18:32:23 +0800

    fix response name

--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,7 @@
 const devState = {
   loginState: {
-    z: '4al80b01d4c528dcd5eb9e094faf86f92cc25g',
+    // z: '4al80b01d4c528dcd5eb9e094faf86f92cc25g',
+    z: '',
     // z: '8b96bc7fba4c1176b3fc0861e94f22465c0f6a',
     gender: '1',
     username: 'Wending',
