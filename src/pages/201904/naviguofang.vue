21aa6eaf053c0eef29c92b439cb02da9f0d0cc46
Anluis <zhengwending@jingfree.com>
2019-04-12 15:30:48 +0800

    reset

---
+++ src/pages/201904/naviguofang.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="navi-root"/>
+</template>
+
+<script>
+const saasUnlockUrl =
+  "http://h5.xingstation.com/marketing/unlockguofang?mkey=c241sa7m&mcode=v8";
+import { getInfoById, splitParms } from "services";
+export default {
+  created() {
+    const { id } = this.$route.query;
+    if (!id) {
+      return;
+    } else {
+      getInfoById(id).then(r => {
+        if (r.parms) {
+          const params = splitParms(r.parms);
+          console.log(params);
+          if (params.hasOwnProperty("rp")) {
+            console.log("ok");
+            const { id } = this.$route.query;
+            window.location.href = `http://h5.xingstation.com/marketing/guofang?id=${id}`;
+          } else {
+            window.location.href = saasUnlockUrl;
+          }
+        } else {
+          window.location.href = saasUnlockUrl;
+        }
+      });
+    }
+  }
+};
+</script>
