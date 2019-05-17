5359d81bffb26ba36b621c5b12d71a2afb9d6114
daisyluara <1481795291@qq.com>
2019-05-17 17:41:14 +0800

    add

--- src/pages/201905/wuyueLetter.vue
+++ src/pages/201905/wuyueLetter.vue
@@ -722,7 +722,7 @@ export default {
             this.ownList.photo = canvas.toDataURL('image/png')
             let base64String = this.ownList.photo.split(",")[1];
             // alert('base64:' + base64String.length);
-            // this.initQiniu()
+            this.initQiniu()
             this.page2 = false
             this.page3 = true
           }
